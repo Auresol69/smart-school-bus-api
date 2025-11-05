6101ba3c24c9f8855864c1e29f15e9c5cede22ce
Auresol69 <quocbao962005@gmail.com>
2025-11-06 02:13:46 +0700

    thiet lap lai cach de join 1 room cu the cho tung trip, them driver:start_trip de xac nhan khoi hanh (di kem voi socket.tripId), fix driver:update_location sao cho toi uu hoa hon bang cach bo await cho viec update Location Bus, them kiem tra neu khong co authHeader & apiKey

--- services/initializeSocket.service.js
+++ services/initializeSocket.service.js
@@ -3,6 +3,8 @@ const AppError = require('../utils/appError');
 const User = require('../models/user.model');
 const Bus = require('../models/bus.model');
 const Location = require('../models/location.model');
+const Trip = require('../models/trip.model');
+const Student = require('../models/student.model');
 
 /**
  * Initializes Socket.IO event listeners and middleware.
@@ -52,6 +54,9 @@ module.exports = (io) => {
                 socket.bus = bus;
                 return next();
             }
+            else {
+                return next(new AppError('Authentication error: No credentials provided.', 401));
+            }
         } catch (error) {
             return next(new AppError('Authentication error: Invalid credentials.', 401));
         }
@@ -69,8 +74,69 @@ module.exports = (io) => {
 
             if (user.role === 'Admin' || user.role === 'Manager') {
                 socket.join(`receive_notification`);
-                socket.join('live-map');
+                // Khong join o day de tranh tinh trang xem dong thoi 300 xe :)))
+                // socket.join('live-map');
             }
+
+            // để đăng ký nhận thông báo cho chuyến đi đó.
+            socket.on('join_trip_room', async (tripId) => {
+
+                try {
+                    let isAllowed = false;
+                    let tripExists = false;
+
+                    if (user.role === 'Admin' || user.role === 'Manager') {
+                        const trip = await Trip.findById(tripId).select('_id');
+
+                        if (trip) {
+                            isAllowed = true;
+                            tripExists = true;
+                        }
+                    }
+
+                    else if (user.role === 'Parent') {
+                        // Xu ly tac vu check xem co con minh trong chuyen do khong
+
+                        const userStudents = await Student.find({ parentId: user._id }).select('_id');
+
+                        if (userStudents.length > 0) {
+                            const studentIds = userStudents.map(s => s._id);
+
+                            const trip = await Trip.findOne(
+                                {
+                                    _id: tripId,
+                                    'studentStops.studentId': { $in: studentIds }
+                                }
+                            ).select('_id');
+
+                            if (trip) {
+                                isAllowed = true;
+                                tripExists = true;
+                            }
+                        }
+                    }
+
+                    if (isAllowed) {
+                        socket.rooms.forEach(room => {
+                            if (room.startsWith('trip_'))
+                                socket.leave(room);
+                        });
+                        socket.join(`trip_${tripId}`);
+                    }
+
+                    else {
+                        if (!tripExists) {
+                            console.log(`User ${user.id} BỊ TỪ CHỐI (Trip không tồn tại): ${tripId}`);
+                        } else {
+                            console.log(`User ${user.id} BỊ TỪ CHỐI (Không có quyền): ${tripId}`);
+                        }
+                    }
+
+                } catch (error) {
+                    console.error(`Lỗi khi ${socket.user.id} join phòng trip_${tripId}:`, error);
+                }
+
+            });
 
             socket.on('disconnect', () => {
                 console.log(`Một NGƯỜI XEM đã ngắt kết nối: ${socket.id} (UserId: ${user.id})`); // Tieng viet cho de hieu
@@ -86,19 +152,67 @@ module.exports = (io) => {
 
             // Server (backend): Nhận tọa độ đó và emit xuống cho (người đang xem bản đồ).
 
+            // Xe buýt phải emit sự kiện này 1 LẦN KHI BẮT ĐẦU CHUYẾN
+            socket.on('driver:start_trip', async (data) => {
+                try {
+                    const tripId = data.tripId;
+                    const busId = socket.bus.id;
+
+                    const trip = await Trip.findById(tripId);
+
+                    if (!trip)
+                        return socket.emit('trip:error', 'Trip ID không tồn tại.');
+
+                    if (trip.busId.toString() !== busId.toString())
+                        return socket.emit('trip:error', 'Xe buýt không được gán cho chuyến này.');
+
+                    if (trip.status !== 'NOT_STARTED') {
+                        // Có thể không phải lỗi, chỉ cần báo là đã chạy rồi
+                        console.log(`Xe buýt ${busId} đã resume chuyến ${trip._id.toString()}`);
+                    } else {
+                        // Tac vu bat buoc => MUST AWAIT (tranh race condition)
+                        trip.status = 'IN_PROGRESS';
+                        await trip.save();
+                    }
+
+                    socket.tripId = trip._id.toString();
+                    console.log(`Xe buýt ${busId} đã BẮT ĐẦU chuyến ${socket.tripId}`);
+                    socket.emit('trip:started_successfully');
+
+                } catch (error) {
+                    // Nếu .save() hoặc .findById() bị lỗi, nó sẽ nhảy vào đây
+                    console.error(`Lỗi khi xe ${socket.bus.id} bắt đầu chuyến ${data.tripId}:`, error.message);
+                    socket.emit('trip:error', 'Lỗi server, không thể bắt đầu chuyến đi.');
+                }
+            });
+
             // ⚠️ QUAN TRỌNG: Không cho join bất kỳ phòng nào cả
-            socket.on('gps-ping', async (data) => {
+            socket.on('driver:update_location', async (data) => {
                 // data format: {busId: '', coords: {latitude: '',longtitude: ''}}
 
                 // Chỉ tin vào 'bus' đax được xác thực sau Middleware tren
                 const busId = socket.bus.id;
-                const updatedBus = await Bus.updateCurrentStatus(busId, data.coords);
+                const validatedTripId = socket.tripId;
 
+                if (!validatedTripId) {
+                    return; // Bỏ qua nếu xe chưa bắt đầu chuyến (start_trip)
+                }
+
+                // Uu tien 2 => KHONG DUNG await de tranh tac nghen
+                Bus.updateCurrentStatus(busId, data.coords)
+                    .catch(err => console.error(`Lỗi cập nhật status bus ${busId}:`, err));
+
+                // Uu tien 1
                 // Gui cho nhung ai dang trong phong live-map VA dang coi map
-                io.to('live-map').emit('bus-moved', updatedBus);
+                io.to(`trip_${validatedTripId}`).emit('bus:location_changed', data.coords);
 
                 // Chi nen su dung khi can data len bao cao
-                // await Location.saveHistory(data.busId, data.coords);
+                // await Location.saveHistory(busId, data.coords);
+            });
+
+            // Da toi 1 tram
+            socket.on('driver:arrived_at_station', async (data) => {
+
             });
 
             socket.on('disconnect', () => {
